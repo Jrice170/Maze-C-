42b0a7a6c9fd1b140969dc3e2d66f0176e416072
jmr3by <jmr3by@mst.edu>
2019-02-21 21:55:29 -0600

    Replace pa03.cpp

--- pa03.cpp
+++ pa03.cpp
@@ -6,10 +6,48 @@
 
 #include "maze.h"
 
+//str.erase(std::remove(str.begin(),str.end(), '\n'), str.end());
+
+
 
 int main()
 {
 
+    string * Maze;
+    ///  number of mazes solved
+    int rows;
+    int pos_col,pos_row;
+    cout<<"Number of rows "<<endl;
+    cin>>rows;
+    int map_number = 0;
+    while(rows !=0)
+    {
+
+        Maze = build_matrix(rows);
+        cin.ignore();
+        fill_matrix(Maze,rows);
+        find_start(Maze,rows,pos_col,pos_row); /// stating position for neo
+        if(find_exit(Maze,pos_col,pos_row))
+        {
+
+            cout<<"Map "<<map_number<<" -- "<<"Solution found:"<<endl;
+
+        }
+        else
+        {
+
+            cout<<"Map "<<map_number<<" -- "<<"No solution found:"<<endl;
+
+        }
+
+        print_matrix(Maze,rows);
+
+
+        delete_matrix(Maze);
+
+        cin>>rows;
+        map_number++;
+    }
+
     return 0;
 }
-
